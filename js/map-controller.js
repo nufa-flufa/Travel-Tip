1857385850e17ac25f7f3b947e91a3e0fb5724c8
ShacharDf <shaars7@gmail.com>
2021-02-17 15:40:44 +0200

    addes address search and axios lib

--- js/map-controller.js
+++ js/map-controller.js
@@ -1,14 +1,21 @@
-import { mapService } from './services/map-service.js';
-import { locationService } from './services/location-service.js';
+import {
+    mapService
+} from './services/map-service.js';
+import {
+    locationService
+} from './services/location-service.js';
 
 var gMap;
-console.log('Main!');
-
-mapService.getLocs()
+mapService.getLocs() // take the loc array from the map service
     .then(locs => console.log('locs', locs))
 
+window.goToAdress = goToAdress;
+window.currPosition = currPosition;
+
+
+
 window.onload = () => {
-    
+
     document.querySelector('.btn').addEventListener('click', (ev) => {
         console.log('Aha!', ev.target);
         panTo(35.6895, 139.6917);
@@ -16,7 +23,10 @@ window.onload = () => {
 
     initMap()
         .then(() => {
-            addMarker({ lat: 32.0749831, lng: 34.9120554 });
+            addMarker({
+                lat: 32.0749831,
+                lng: 34.9120554
+            });
         })
         .catch(() => console.log('INIT MAP ERROR'));
 
@@ -36,18 +46,21 @@ function initMap(lat = 32.0749831, lng = 34.9120554) {
             console.log('google available');
             gMap = new google.maps.Map(
                 document.querySelector('#map'), {
-                center: { lat, lng },
-                zoom: 15
-            })
+                    center: {
+                        lat,
+                        lng
+                    },
+                    zoom: 15
+                })
             console.log('Map!', gMap);
         })
 }
 
-function addMarker(loc) {
+function addMarker(loc, text) {
     var marker = new google.maps.Marker({
         position: loc,
         map: gMap,
-        title: 'Hello World!'
+        title: text
     });
     return marker;
 }
@@ -57,18 +70,42 @@ function panTo(lat, lng) {
     gMap.panTo(laLatLng);
 }
 
+function currPosition() {
+    return getPosition()
+        .then(pos => {
+            const latLng = {
+                lat: pos.coords.latitude,
+                lng: pos.coords.longitude
+            }
+            console.log('User position is:', latLng);
+            return latLng;
+        })
+        .catch(err => {
+            console.log('err!!!', err);
+        })
+
+}
+
+function goToAdress() {
+    const address = document.querySelector('.search-address').value;
+    if (address) {
+        mapService.getLatLngByAdress(address);
+    }
+
+}
+
 // This function provides a Promise API to the callback-based-api of getCurrentPosition
 function getPosition() {
     console.log('Getting Pos');
     return new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(resolve, reject)
+        navigator.geolocation.getCurrentPosition(resolve, reject);
     })
 }
 
 
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = 'AIzaSyA17BYjyDxu05k6Xn8hWDDbMEdoghMsdGU'; //TODO: Enter your API Key
+    const API_KEY = 'AIzaSyA17BYjyDxu05k6Xn8hWDDbMEdoghMsdGU'; //Enter your API Key
     var elGoogleApi = document.createElement('script');
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
     elGoogleApi.async = true;
@@ -79,6 +116,3 @@ function _connectGoogleApi() {
         elGoogleApi.onerror = () => reject('Google script failed to load')
     })
 }
-
-
-
